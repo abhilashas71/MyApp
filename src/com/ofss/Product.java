8f4815163ec5e120436f86263309661fa23715bc
Abhilasha <abhilashas71@gmail.com>
2020-08-05 10:44:19 +0530

    adding files

---
+++ src/com/ofss/Product.java
@@ -0,0 +1,30 @@
+package com.ofss;
+
+public class Product {
+	String productName;
+	double price;
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(String productName, double price) {
+		super();
+		this.productName = productName;
+		this.price = price;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
